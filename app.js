8a436d03f0c512bfe8525c7865aaf645dbc904ef
Rou <rougirel.stephanie@gmail.com>
2020-05-08 11:27:52 +0200

    bouton logout ok

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require ("express")
+const   express = require ("express")
 const    exphbs = require("express-handlebars");
 const    mongoose = require ("mongoose");
 const    bodyParser = require ("body-parser") ;
